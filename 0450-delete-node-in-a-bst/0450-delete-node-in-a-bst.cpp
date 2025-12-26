138966eef568a48c27a927e7140c79dd04748d17
Abhishek kumar <abishek2002tk@gmail.com>
2025-12-26 19:23:37 +0530

    Time: 0 ms (100%), Space: 34.3 MB (72.16%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* succesor(TreeNode* root) {
+        TreeNode* curr = root;
+        if(root->right) curr = curr->right;
+
+        while(curr->left != NULL) {
+            curr = curr->left;
+        }
+        return curr;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) return NULL;
+        TreeNode* curr = root;
+
+        
+        if(key < curr->val){
+            curr->left = deleteNode(curr->left, key);
+        }
+        else if(key > curr->val){
+            curr->right = deleteNode(curr->right, key);
+        }
+        else{
+            // 0 or 1 child
+            if(curr->left == NULL){
+                TreeNode* temp = curr->right;
+                delete curr;
+                return temp;
+            }
+            else if(curr->right == NULL){
+                TreeNode* temp = curr->left;
+                delete curr;
+                return temp;
+            }
+            else{
+                TreeNode* succ = succesor(curr);
+                curr->val = succ->val;
+                root->right = deleteNode(root->right, succ->val);
+            }
+        }
+        
+        return root;
+    }
+};
