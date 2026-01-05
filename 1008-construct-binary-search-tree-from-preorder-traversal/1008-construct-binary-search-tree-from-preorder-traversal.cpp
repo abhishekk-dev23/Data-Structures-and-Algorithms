f402a2ff8e3d2b36a7af63c9a0ffc1d7c68019b3
Abhishek kumar <abishek2002tk@gmail.com>
2026-01-05 20:08:07 +0530

    Time: 0 ms (100%), Space: 16.6 MB (21.35%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,39 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* addChild(TreeNode* &root, int d) {
-        if(root == NULL) {
-            root = new TreeNode(d);
-            return root;
-        };
+    TreeNode* createTree(vector<int> &preorder, int &i, unordered_map<int, int> &mp, int s, int e) {
+        if(s > e || i >= preorder.size()) return NULL;
+        int rootVal = preorder[i++];
+        TreeNode* root = new TreeNode(rootVal);
+        int mid = mp[rootVal];
 
-        if(d < root->val) {
-            root->left = addChild(root->left, d);
-        }
-        else{
-            root->right = addChild(root->right, d);
-        }
+        root->left = createTree(preorder, i, mp, s, mid-1);
+        root->right = createTree(preorder, i, mp, mid+1, e);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root = NULL;
-
+        if(preorder.size() <= 0) return NULL;
+        vector<int> pre = preorder;
+        sort(preorder.begin(), preorder.end());
+        unordered_map<int, int> mp;
         for(int i=0; i<preorder.size(); i++) {
-            root = addChild(root, preorder[i]);
+            mp[preorder[i]] = i;
         }
-        return root;
+        int preOrdIdx = 0;
+        TreeNode* ans = createTree(pre, preOrdIdx, mp, 0, preorder.size()-1);
+        return ans;
     }
+    
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
