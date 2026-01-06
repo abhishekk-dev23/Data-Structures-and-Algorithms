5f1deb68651ef4d29a702945396c13f8c93ef0da
Abhishek kumar <abishek2002tk@gmail.com>
2026-01-06 23:18:18 +0530

    Time: 0 ms (100%), Space: 60.9 MB (27.43%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -10,27 +10,35 @@
  * };
  */
 class Solution {
-public:
-    void InOrder(TreeNode* root, vector<int> &ans) {
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+private:
+    void inorder(TreeNode* root) {
         if(root == NULL) return;
-        InOrder(root->left, ans);
-        ans.push_back(root->val);
-        InOrder(root->right, ans);
-    }
-    void CreateTree(TreeNode* root, vector<int> &ans, int &i) {
-        if(root == NULL) return;
-        if(i >= ans.size()) return;
+        inorder(root->left);
 
-        CreateTree(root->left, ans, i);
-        root->val = ans[i++];
-        CreateTree(root->right, ans, i);
+        if(prev != NULL && (root->val < prev->val)){
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            }
+            else{
+                last = root;
+            }
+        }
+        prev = root;
+
+        inorder(root->right);
     }
-    void recoverTree(TreeNode* &root) {
-        vector<int> ans;
-        if(root == NULL) return;
-        InOrder(root, ans);
-        sort(ans.begin(), ans.end());
-        int i=0;
-        CreateTree(root, ans, i);
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val, last->val);
+        else if(first && middle) swap(first->val, middle->val);
     }
 };
