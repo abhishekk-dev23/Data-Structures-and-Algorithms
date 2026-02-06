973579625d809bb36830b761bf99cecedf1ce232
Abhishek kumar <abishek2002tk@gmail.com>
2026-02-06 22:09:36 +0530

    Time: 28 ms (88.14%), Space: 84.7 MB (82.37%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,15 +8,16 @@ public:
         while(r < fruits.size()) {
             freq[fruits[r]]++;
 
-            while(freq.size() > 2) {
+            if(freq.size() > 2) {
                 freq[fruits[l]]--;
                 if(freq[fruits[l]] == 0) {
                     freq.erase(fruits[l]);
                 }
                 l++;
             }
-            
-            max_len = max(max_len, r-l+1);
+            if(freq.size() <= 2){
+                max_len = max(max_len, r-l+1);
+            }
             r++;
         }
         return max_len;
