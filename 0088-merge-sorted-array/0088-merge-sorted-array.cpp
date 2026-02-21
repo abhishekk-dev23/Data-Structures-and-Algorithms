c72be1ca5369b324af9fd91043a345d9325da0e3
Abhishek kumar <abishek2002tk@gmail.com>
2026-02-21 12:28:06 +0530

    Time: 0 ms (100%), Space: 12.2 MB (71.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    void helperFun(vector<int>& nums, int x, int size) {
-        nums[size] = x;
-        for(int j=size; j>0; j--) {
-            if(nums[j] < nums[j-1]) {
-                swap(nums[j], nums[j-1]);
-            }
-        } 
-    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<nums2.size(); i++) {
-            int x = nums2[i];
-            helperFun(nums1, x, m++);
+        int lastIdx1 = m - 1;
+        int lastIdx2 = n - 1;
+        int lastIdx = m+n-1;
+
+        while(lastIdx1 >= 0 && lastIdx2 >= 0) {
+            if(nums1[lastIdx1] > nums2[lastIdx2]) {
+                nums1[lastIdx] = nums1[lastIdx1];
+                lastIdx1--;
+            }
+            else{
+                nums1[lastIdx] = nums2[lastIdx2];
+                lastIdx2--;
+            }
+            lastIdx--;
+        }
+        while(lastIdx2 >= 0) {
+            nums1[lastIdx] = nums2[lastIdx2];
+            lastIdx--;
+            lastIdx2--;
         }
     }
 };
