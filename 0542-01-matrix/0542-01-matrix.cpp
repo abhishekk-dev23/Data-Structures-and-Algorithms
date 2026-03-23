0f671d112b6bf42bdf92af2ca0ed9506f07b3a8e
Abhishek kumar <abishek2002tk@gmail.com>
2026-03-23 20:10:50 +0530

    Time: 11 ms (84.48%), Space: 34.7 MB (83.5%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,40 +4,36 @@ public:
         int r = mat.size();
         int c = mat[0].size();
                 //      {{x, y}, dis}
-        queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(r, vector<int>(c, 0));
+        queue<pair<int, int>> q;
+        vector<vector<int>> res(r, vector<int>(c, INT_MAX));
 
         for(int i=0; i<r; i++) {
             for(int j=0; j<c; j++) {
                 if(mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 1;
+                    q.push({i, j});
+                    res[i][j] = 0;
                 }
             }
         }
-        vector<vector<int>> res(r, vector<int>(c));
 
-        int dis[4][2] = {{+1, 0}, {0, +1}, {-1, 0}, {0, -1}};
+        int dir[4][2] = {{+1, 0}, {0, +1}, {-1, 0}, {0, -1}};
 
         while(!q.empty()) {
             auto node = q.front();
             q.pop();
-            int x = node.first.first;
-            int y = node.first.second;
-            int min_dis = node.second;
+            int x = node.first;
+            int y = node.second;
 
-            res[x][y] = min_dis;
-            vis[x][y] = 1;
             for(int i=0; i<4; i++) {
-                int finX = x + dis[i][0];
-                int finY = y + dis[i][1];
+                int finX = x + dir[i][0];
+                int finY = y + dir[i][1];
 
                 if(finX >= 0 && finX < r && finY >= 0 &&
-                    finY < c && 
-                    !vis[finX][finY]) {
-                        vis[finX][finY] = 1;
-                        q.push({{finX, finY}, min_dis+1});
-                        
+                    finY < c) {
+                    if(res[finX][finY] > res[x][y] + 1) {
+                        res[finX][finY] = res[x][y] + 1;
+                        q.push({finX, finY});
+                    }
                 }
             }
         }
