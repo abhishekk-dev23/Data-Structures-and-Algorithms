ea5b8ab1de7d95ed44c56eac6b3176b5209045ef
Abhishek kumar <abishek2002tk@gmail.com>
2026-03-24 20:07:21 +0530

    Time: 7 ms (73.2%), Space: 28.8 MB (94.91%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -4,28 +4,27 @@ public:
         int r = grid.size();
         int c = grid[0].size();
 
-        vector<vector<int>> vis(r, vector<int>(c, 0));
         queue<pair<int, int>> q;
         // left and right
         for(int i=0; i<r; i++) {
             if(grid[i][0] == 1) {
                 q.push({i, 0});
-                vis[i][0] = 1;
+                grid[i][0] = 0;
             }
             if(grid[i][c-1] == 1) {
                 q.push({i, c-1});
-                vis[i][c-1] = 1;
+                grid[i][c-1] = 0;
             }
         }
         // top and bottom
-        for(int j=0; j<c; j++) {
+        for(int j=1; j<c-1; j++) {
             if(grid[0][j] == 1) {
                 q.push({0, j});
-                vis[0][j] = 1;
+                grid[0][j] = 0;
             }
             if(grid[r-1][j] == 1) {
                 q.push({r-1, j});
-                vis[r-1][j] = 1;
+                grid[r-1][j] = 0;
             }
         }
 
@@ -40,9 +39,9 @@ public:
                 int ny = y + dir[i][1];
 
                 if(nx < r && nx >= 0 && ny < c && ny >= 0 &&
-                    !vis[nx][ny] && grid[nx][ny] == 1) {
+                    grid[nx][ny] == 1) {
 
-                        vis[nx][ny] = 1;
+                        grid[nx][ny] = 0;
                         q.push({nx, ny});
                     }
             }
@@ -51,7 +50,7 @@ public:
         int cnt = 0;
         for(int i=0; i<r; i++) {
             for(int j=0; j<c; j++) {
-                if(!vis[i][j] && grid[i][j] == 1)
+                if(grid[i][j] == 1)
                     cnt++;
             }
         }
