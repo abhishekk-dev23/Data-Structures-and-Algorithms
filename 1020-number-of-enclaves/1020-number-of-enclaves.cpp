e3879330df933dd55f036d925ab13d77f649adc9
Abhishek kumar <abishek2002tk@gmail.com>
2026-03-24 20:16:37 +0530

    Time: 3 ms (94.76%), Space: 34.1 MB (51.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,49 +1,47 @@
 class Solution {
+private:
+    void dfs(int x, int y, vector<vector<int>> &grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+
+            if(nx < r && nx >= 0 && ny < c && ny >= 0 &&
+                grid[nx][ny] == 1) {
+
+                    grid[nx][ny] = 0;
+                    dfs(nx, ny, grid);
+                }
+        }
+    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
 
-        queue<pair<int, int>> q;
         // left and right
         for(int i=0; i<r; i++) {
             if(grid[i][0] == 1) {
-                q.push({i, 0});
                 grid[i][0] = 0;
+                dfs(i, 0, grid);
             }
             if(grid[i][c-1] == 1) {
-                q.push({i, c-1});
                 grid[i][c-1] = 0;
+                dfs(i, c-1, grid);
             }
         }
         // top and bottom
         for(int j=1; j<c-1; j++) {
             if(grid[0][j] == 1) {
-                q.push({0, j});
                 grid[0][j] = 0;
+                dfs(0, j, grid);
             }
             if(grid[r-1][j] == 1) {
-                q.push({r-1, j});
                 grid[r-1][j] = 0;
-            }
-        }
-
-        int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-        while(!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
-
-            for(int i=0; i<4; i++) {
-                int nx = x + dir[i][0];
-                int ny = y + dir[i][1];
-
-                if(nx < r && nx >= 0 && ny < c && ny >= 0 &&
-                    grid[nx][ny] == 1) {
-
-                        grid[nx][ny] = 0;
-                        q.push({nx, ny});
-                    }
+                dfs(r-1, j, grid);
             }
         }
 
