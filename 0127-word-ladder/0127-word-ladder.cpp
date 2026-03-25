fe3737298e1a71c0ad68800c14aceea34a286e50
Abhishek kumar <abishek2002tk@gmail.com>
2026-03-25 19:45:42 +0530

    Time: 298 ms (28.73%), Space: 21.2 MB (42.55%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    bool isPresent(string word, set<string> &str) {
+        return str.count(word);
+    }
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+        set<string> str;
+
+        for(int i=0; i<n; i++) {
+            str.insert(wordList[i]);
+        }
+        if(!str.count(endWord)) return 0;
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+
+        while(!q.empty()) {
+            auto [s, l] = q.front();
+            q.pop();
+
+            if(s == endWord) return l;
+
+            // checking all value a-z
+            for(int i=0; i<s.size(); i++) {
+                char original = s[i];
+
+                for(char ch='a'; ch<='z'; ch++) {
+                    s[i] = ch;
+                    if(str.count(s)) {
+                        q.push({s, l+1});
+                        str.erase(s);
+                    }
+                }
+                s[i] = original;
+            }
+        }
+        return 0;
+    }
+};
