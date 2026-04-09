afd9246902e26c0b9e54d8151e0f6a760c49fd0c
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-09 19:45:48 +0530

    Time: 0 ms (100%), Space: 11.5 MB (83.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,25 +6,26 @@ public:
         if(obstacleGrid[0][0] || obstacleGrid[r-1][c-1])
             return 0;
 
-        vector<vector<int>> dp(r, vector<int> (c, -1));
-
-        int obsX = -1, obsY = -1;
+        vector<int> prev(c, 0);
         for(int i=0; i<r; i++) {
+            vector<int> curr(c, 0);
+
             for(int j=0; j<c; j++) {
-                if(i == 0 && j == 0) dp[i][j] = 1;
+                if(i == 0 && j == 0) curr[j] = 1;
                 else{
                     int left = 0, up = 0;
                     if(i > 0) {
-                        if(!obstacleGrid[i-1][j]) up = dp[i-1][j];
+                        if(!obstacleGrid[i-1][j]) up = prev[j];
                     } 
                     if(j > 0) {
-                        if(!obstacleGrid[i][j-1]) left = dp[i][j-1];
+                        if(!obstacleGrid[i][j-1]) left = curr[j-1];
                     }
 
-                    dp[i][j] = left + up;
+                    curr[j] = left + up;
                 }
             }
+            prev = curr;
         }
-        return dp[r-1][c-1];
+        return prev[c-1];
     }
 };
