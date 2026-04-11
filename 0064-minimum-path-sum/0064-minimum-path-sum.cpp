349b4cf3c241b6af078834de189c073364829a99
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-11 17:26:05 +0530

    Time: 0 ms (100%), Space: 16.3 MB (39.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,7 +20,20 @@ public:
         int c = grid[0].size();
         vector<vector<int>> dp(r, vector<int> (c, -1));
         
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
 
-        return findPath(r-1, c-1, grid, dp);
+                    dp[i][j] = grid[i][j] + min(up, left);
+                }
+            }
+        }
+        return dp[r-1][c-1];
     }
 };
