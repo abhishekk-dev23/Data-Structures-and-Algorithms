4fe1c5d58459a5eee566d85fe9e77888e8441bec
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-11 17:28:51 +0530

    Time: 0 ms (100%), Space: 15.9 MB (71.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,39 +1,27 @@
 class Solution {
-private:
-    int findPath(int x, int y, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        
-        if(x == 0 && y == 0) {
-            return dp[x][y] = grid[x][y];
-        }
-        if(dp[x][y] != -1) return dp[x][y];
-
-        int up = INT_MAX, left = INT_MAX;
-        if(x > 0) up = findPath(x-1, y, grid, dp);
-        if(y > 0) left = findPath(x, y-1, grid, dp);
-
-
-        return dp[x][y] = grid[x][y] + min(up, left);
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        vector<vector<int>> dp(r, vector<int> (c, -1));
+        vector<int> prev(c, 0);
         
         for(int i=0; i<r; i++) {
+            vector<int> curr(c, 0);
+
             for(int j=0; j<c; j++) {
                 if(i == 0 && j == 0) {
-                    dp[i][j] = grid[i][j];
+                    curr[j] = grid[i][j];
                 }
                 else{
                     int up = INT_MAX, left = INT_MAX;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
 
-                    dp[i][j] = grid[i][j] + min(up, left);
+                    curr[j] = grid[i][j] + min(up, left);
                 }
             }
+            prev = curr;
         }
-        return dp[r-1][c-1];
+        return prev[c-1];
     }
 };
