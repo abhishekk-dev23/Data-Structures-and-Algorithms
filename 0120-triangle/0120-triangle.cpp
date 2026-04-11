7fcebe0099e3f43e4fd80f5a97024cca04f93343
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-12 00:03:11 +0530

    Time: 0 ms (100%), Space: 13.4 MB (38%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,30 +1,22 @@
 class Solution {
-private:
-    int findPath(int x, int y, vector<vector<int>>& triangle, vector<vector<int>> &dp) {
-        if(x == 0 && y == 0) {
-            return dp[x][y] = triangle[x][y];
-        }
-
-        if(dp[x][y] != -1) return dp[x][y];
-        
-        int dig = INT_MAX, up = INT_MAX;
-        if(x > 0 && y <= x-1) up = findPath(x-1, y, triangle, dp);
-        if(x > 0 && y > 0) dig = findPath(x-1, y-1, triangle, dp);
-        
-        return dp[x][y] = triangle[x][y] + min(dig, up);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int r = triangle.size();
         int c = triangle[r-1].size();
 
         vector<vector<int>> dp(r, vector<int> (c, -1));
-        int sum = 0;
-        int min_sum = INT_MAX;
-        for(int i=0; i<c; i++) {
-            sum = findPath(r-1, i, triangle, dp);
-            min_sum = min(min_sum, sum);
+        for(int i=0; i<c; i++) 
+            dp[r-1][i] = triangle[r-1][i];
+        
+        for(int i=r-2; i>=0; i--) {
+            for(int j=i; j>=0; j--) {
+                int down = INT_MAX, dig = INT_MAX;
+                down = triangle[i][j] + dp[i+1][j];
+                dig = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down, dig);
+            }
         }
-        return min_sum;
+        return dp[0][0];
     }
 };
