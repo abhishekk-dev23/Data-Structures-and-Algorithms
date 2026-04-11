60fd9ede6f19de6ebbbd6e98850e11e1b256810e
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-12 00:13:00 +0530

    Time: 7 ms (3.55%), Space: 12.8 MB (59.51%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,12 +4,11 @@ public:
         int r = triangle.size();
         int c = triangle[r-1].size();
 
-        vector<int> next(c, 0);
+        vector<int> next(c, 0), curr(c, 0);
         for(int i=0; i<c; i++) 
             next[i] = triangle[r-1][i];
         
         for(int i=r-2; i>=0; i--) {
-            vector<int> curr(i+1, 0);
 
             for(int j=i; j>=0; j--) {
                 int down = INT_MAX, dig = INT_MAX;
