c624cd4b4780e8725a8b9ee93841475a7e1c0d70
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-19 11:56:01 +0530

    Time: 118 ms (93.75%), Space: 201.6 MB (96.1%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -2,43 +2,31 @@ class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         vector<vector<int>> grid(n, vector<int>(m, 0));
-        vector<vector<int>> best(n, vector<int>(m, 0));
         queue<tuple<int, int, int>> q;
 
-        for(auto &src : sources) {
-            int r = src[0];
-            int c = src[1];
-            int col = src[2];
-    
-            grid[r][c] = col;
-            q.push({r, c, col});
+        sort(sources.begin(), sources.end(), [](vector<int> &a, vector<int> &b){
+            return a[2]>b[2];
+        });
+
+        for(auto& s : sources) {
+            grid[s[0]][s[1]] = s[2];
+            q.push({s[0], s[1], s[2]});
         }
         
         int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!q.empty()) {
-            int sz = q.size();
-            vector<pair<int, int>> vec;
+            auto [x, y, c] = q.front();
+            q.pop();
 
-            for(int k=0; k<sz; k++) {
-                auto [x, y, c] = q.front();
-                q.pop();
-    
-                for(auto& d: dir) {
-                    int nx = x + d[0];
-                    int ny = y + d[1];
-    
-                    if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0) {
-                        if(best[nx][ny] == 0) vec.push_back({nx, ny});
-                        best[nx][ny] = max(best[nx][ny], c);
-                    }
+            for(auto& d: dir) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0) {
+                    grid[nx][ny] = c;
+                    q.push({nx, ny, c});
                 }
-            }
-            
-            for(auto& [x, y]: vec) {
-                grid[x][y] = best[x][y];
-                q.push({x, y, grid[x][y]});
-                best[x][y] = 0;
             }
                   
         }
