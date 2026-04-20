232a514d6e7fd94d96e0c12d3eb9475fc6443e75
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-20 20:30:03 +0530

    Time: 5 ms (41.35%), Space: 9.5 MB (40.83%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,28 +1,18 @@
 class Solution {
-private: 
-    bool checkVow(string word, int s, int e) {
-        string str = "aeiou";
-        set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-        set<char> st;
-        for(int i=s; i<=e; i++) {
-            if(vowels.find(word[i]) == vowels.end()) return false;
-            st.insert(word[i]);
-        }
-        
-        return st.size() == 5;
-    }
 public:
     int countVowelSubstrings(string word) {
         int n = word.size();
         int cnt = 0;
+        set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
 
         for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                if(j - i + 1 >= 5 && checkVow(word, i, j)) {
-                    cnt++;
-                }
-            }
+            set<char> st;
             
+            for(int j=i; j<n; j++) {
+                if(vowels.find(word[j]) == vowels.end()) break; 
+                st.insert(word[j]);
+                if(st.size() == 5) cnt++;
+            }
         }
         return cnt;
     }
