780256117478d917a34b3c92f74e00429c7c4520
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-23 00:11:58 +0530

    Time: 11 ms (49.2%), Space: 19 MB (30.48%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,27 +1,21 @@
 class Solution {
-private:
-    bool findString(string word, vector<string>& dic) {
-        
-        for(int i=0; i<dic.size(); i++) {
-            string str = dic[i];
-            int cnt = 0;
-            for(int j=0; j<str.size(); j++) {
-                if(word[j] != str[j]) {
-                    cnt++;
-                }
-                if(cnt > 2)  break;
-            }
-            if(cnt <= 2) return true;
-        }
-        return false;
-    }
 public:
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         vector<string> res;
 
         for(auto it: queries) {
-            if(findString(it, dictionary)) {
-                res.push_back(it);
+            string wo = it;
+            for(int i=0; i<dictionary.size(); i++) {
+                int cnt = 0;
+                string str = dictionary[i];
+                for(int j=0; j<str.size(); j++) {
+                    if(str[j] != wo[j]) cnt++;
+                    if(cnt > 2) break;
+                }
+                if(cnt <= 2) {
+                    res.push_back(it);
+                    break;
+                };
             }
         }
         return res;
