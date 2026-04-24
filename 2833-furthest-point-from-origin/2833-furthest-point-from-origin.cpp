1662635567bb4b5bda2d12c5463aaab502ed9162
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-24 18:59:24 +0530

    Time: 3 ms (6.81%), Space: 9.3 MB (6.23%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
         int n = moves.size();
-        unordered_map<char, int> mpp;
+        vector<int> mpp(2, 0);
         for(auto& it: moves) {
-            mpp[it]++;
+            if(it == 'L')  mpp[0]++;
+            else if(it == 'R')  mpp[1]++;
         }
-        int _path = max(mpp['L'], mpp['R']);
 
         int path = 0;
-        int _val = mpp['L'] > mpp['R'] ? -1 : 1;
+        int _val = mpp[0] > mpp[1] ? -1 : 1;
         
         for(int i=0; i<n; i++) {
             if(moves[i] == 'L') path -= 1;
