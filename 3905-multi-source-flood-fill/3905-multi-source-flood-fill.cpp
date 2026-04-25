23f657ad50925ee14315d3a5f0f5e34cb791872a
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-25 10:52:41 +0530

    Time: 459 ms (32.09%), Space: 303.4 MB (29.02%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -2,31 +2,52 @@ class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         vector<vector<int>> grid(n, vector<int>(m, 0));
-        queue<tuple<int, int, int>> q;
+        queue<pair<pair<int,int>, int>> q;
 
-        sort(sources.begin(), sources.end(), [](vector<int> &a, vector<int> &b){
-            return a[2]>b[2];
-        });
+        auto lenqavirod = sources;
 
-        for(auto& s : sources) {
-            grid[s[0]][s[1]] = s[2];
-            q.push({s[0], s[1], s[2]});
+        for(auto &src : sources) {
+            int r = src[0];
+            int c = src[1];
+            int col = src[2];
+    
+            grid[r][c] = col;
+            q.push({{r,c}, col});
         }
         
         int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!q.empty()) {
-            auto [x, y, c] = q.front();
-            q.pop();
 
-            for(auto& d: dir) {
-                int nx = x + d[0];
-                int ny = y + d[1];
+            int sz = q.size();
+    
+            map<pair<int,int>, int> mp;
 
-                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0) {
-                    grid[nx][ny] = c;
-                    q.push({nx, ny, c});
+            for(int k=0; k<sz; k++) {
+                auto front = q.front();
+                q.pop();
+    
+                int x = front.first.first;
+                int y = front.first.second;
+                int c = front.second;
+    
+                for(int i=0;i<4;i++) {
+                    int nx = x + dir[i][0];
+                    int ny = y + dir[i][1];
+    
+                    if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0) {
+                        mp[{nx,ny}] = max(mp[{nx,ny}], c);
+                    }
                 }
+            }
+            
+            for(auto &it : mp) {
+                int x = it.first.first;
+                int y = it.first.second;
+                int c = it.second;
+    
+                grid[x][y] = c;
+                q.push({{x,y}, c});
             }
                   
         }
